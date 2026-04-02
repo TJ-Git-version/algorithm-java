0d85f0e7d903eaf0340b21b24d3121c7f0487eae
jiangjunfeng <jiangjunfeng@qq.com>
2026-04-02 17:43:11 +0800

    刷算法：49.字母异位词分组

---
+++ src/main/java/com/surfur/algorithm/hots/哈希/字母异位词分组_49.java
@@ -0,0 +1,42 @@
+package com.surfur.algorithm.hots.哈希;
+
+import java.util.*;
+
+/**
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ *
+ * 示例 1:
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * 解释：
+ * 在 strs 中没有字符串可以通过重新排列来形成 "bat"。
+ * 字符串 "nat" 和 "tan" 是字母异位词，因为它们可以重新排列以形成彼此。
+ * 字符串 "ate" ，"eat" 和 "tea" 是字母异位词，因为它们可以重新排列以形成彼此。
+ * 示例 2:
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ */
+public class 字母异位词分组_49 {
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = groupAnagrams(strs);
+        System.out.println(lists);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs)  {
+        Map<String, List<String>> groupAnagramMap = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String key = Arrays.toString(charArray);
+            groupAnagramMap.putIfAbsent(key, new ArrayList<>());
+            groupAnagramMap.get(key).add(str);
+        }
+        return groupAnagramMap.values().stream().toList();
+    }
+}
